6fb5dca7b89a28564c7477389b813d57fc360e7f
Samet Koyuncu <sametkoyuncu@live.com>
2022-12-31 12:57:20 +0300

    Initial Commit

---
+++ TodoListSwiftUI/TodoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftUIApp.swift
+//  TodoListSwiftUI
+//
+//  Created by Samet Koyuncu on 31.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
